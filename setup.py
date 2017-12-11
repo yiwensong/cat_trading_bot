360d1be81c9a49f62f2fe276924c4dac84d9f636
Yiwen Song <songzgy@gmail.com>
2017-12-11 10:59:36 -0800

    Update and rename __init__.py to setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+  name='cat_trading_bot',
+  version='0.0.0',
+  license='Apache',
+  packages=setuptools.find_packages(exclude=['tests*']),
+  install_requires=[
+  ],
+  classifiers=[
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3.6',
+  ],
+  entry_points={
+  },
+)
