b897bd22680832f5ed5343b2fc874f35186bfdb9
Yiwen Song <songzgy@gmail.com>
2017-12-11 12:42:27 -0800

    Update ethtools.py

--- cat_trading_bot/ethtools.py
+++ cat_trading_bot/ethtools.py
@@ -1,5 +1,17 @@
 from ethereum import utils
+from ethereum.transactions import Transaction
+import web3
 import os
+
+
+class EthWallet():
+    def __init__(self, key=None):
+        if not key:
+            key = generate_key()
+        self.key = key
+        self.raw_addr = utils.privtoaddr(private_key)
+        self.acct_addr = utils.checksum_encode(raw_address)
+
 
 def generate_key():
     private_key = utils.sha3(os.urandom(4096))
