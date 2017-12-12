41702f4bd95679d503e907db5ee1e3124a76860c
Yiwen Song <songzgy@gmail.com>
2017-12-11 21:59:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 import setuptools
 
 setuptools.setup(
-  name='cat_trading_bot',
-  version='0.0.0',
-  license='Apache',
-  packages=setuptools.find_packages(exclude=['tests*']),
-  install_requires=[
-  ],
-  classifiers=[
-    'Natural Language :: English',
-    'Programming Language :: Python :: 3.6',
-  ],
-  entry_points={
-  },
+    name='cat_trading_bot',
+    version='0.0.0',
+    license='Apache',
+    packages=setuptools.find_packages(exclude=['tests*']),
+    install_requires=[
+        os,
+        web3,
+    ],
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+    },
 )
